5c04273c4940d3b0ed1168a80e5128955b22829e
Isaias Velazquez <isa3426@hotmail.com>
2025-08-30 00:13:22 -0300

    Se realizo el punto D del EJ1

---
+++ src/Ejercicio1/MainEjercicio1_a.java
@@ -0,0 +1,32 @@
+package Ejercicio1;
+
+import java.util.ArrayList;
+
+public class MainEjercicio1_a {
+	
+	public static void main (String[] args) {
+		
+		Persona p1 = new Persona();
+		p1.setDni("AA202020");
+		
+		Persona p2 = new Persona();
+		p2.setDni("20202020");
+		
+		ArrayList<Persona> listaPersonas = new ArrayList<Persona>(2);
+		listaPersonas.add(p1);
+		listaPersonas.add(p2);
+		
+		for (Persona persona : listaPersonas) {
+			try 
+			{				
+				Persona.exVerificarDNI(persona.getDni());	
+				System.out.println("Persona agregada correctamente");			
+			}
+			catch(ExVerificarDNI e) 
+			{
+				System.out.println("Persona no agregada por no verificar el DNI");	
+			}
+		}			
+	}
+
+}
